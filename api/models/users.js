a98b46611a39c822d88813b01e42984d0973b2e1
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-08-12 00:19:23 -0400

    on users to get users in request

--- api/models/users.js
+++ api/models/users.js
@@ -8,13 +8,19 @@
 // type
 //if params to be passing  in options
 
-const User = [
-    {
-        id: 0,
-        username: "aimee",
-        password: "7f4455f0fa9a5a03:bce63b8acc9b664cbbe315b4b8692c24f08089e9d3b7a1f8",
-        access_token: '',
-        type: ''
-    }
-]
-module.exports = User
+const GetUser = () => {
+    const User = [
+        {
+            id: 0,
+            username: "aimee",
+            password: "7f4455f0fa9a5a03:bce63b8acc9b664cbbe315b4b8692c24f08089e9d3b7a1f8",
+            access_token: '',
+            type: ''
+        }
+    ]
+    return User;
+}
+module.exports = {
+    users: GetUser()
+
+}
