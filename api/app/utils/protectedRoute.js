cbdcb0eb1eccaaa8f71ae32e4bff2767b315be07
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-08-29 19:19:21 -0400

    protected routes on token

--- api/app/utils/protectedRoute.js
+++ api/app/utils/protectedRoute.js
@@ -1,14 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 const protectedRoute = (req, res, next) => {
-    const { token } = req.headers;
-    //TODO remove userId
-    console.log("TOKEN route===>", req.headers)
+    let { token } = req.headers;
+    token = req.header("Authorization").replace("Bearer ", "")
+    // console.log("TOKEN route===>", req.headers)
     try {
         const { id } = jwt.verify(token, process.env.SECRET);
-        res.userId = id;
-        res.token = token;
-        req.userId = id;
         req.userId = id;
         return next();
     } catch (e) {
